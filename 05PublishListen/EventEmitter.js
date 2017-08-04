d590df6b5c7c8a4b17ac64d21cc68330cf1c092d
yuchao919 <yuchao919@hotmail.com>
2017-08-04 17:39:49 +0800

    add a eventemitter

---
+++ 05PublishListen/EventEmitter.js
@@ -0,0 +1,35 @@
+var EventEmitter = function() {
+  var self = this;
+  this.eventDictionary = {};
+  this.on = function(eventName, callback) {
+    // if (self.eventNameList.indexOf(eventName) === -1) {
+    //   self.eventNameList.push(eventName);
+    // }
+    var eventDictionary = self.eventDictionary;
+    if (!eventDictionary.hasOwnProperty(eventName)) {
+      eventDictionary[eventName] = [callback];
+    } else {
+      eventDictionary[eventName].push(callback);
+    }
+  };
+  this.emit = function(eventName) {
+    var eventDictionary = self.eventDictionary;
+    if (!eventDictionary.hasOwnProperty(eventName)) {
+      throw new Error("Theere is no such evnet");
+    }
+    var args = Array.prototype.slice.call(arguments, 1);
+    eventDictionary[eventName].forEach(callback => {
+      if (typeof callback === "function") {
+        callback.apply(null, args);
+      }
+    });
+  };
+};
+
+var myEmitter = new EventEmitter();
+
+myEmitter.on("connect", (hello, world) => {
+  console.log(hello + " " + world);
+});
+
+myEmitter.emit("connect", "hello", "world");
