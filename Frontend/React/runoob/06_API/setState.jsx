b589d77097090b97f85c654a49946a2b98a08309
yuchao919 <yuchao919@hotmail.com>
2018-03-02 22:42:40 +0800

    add React demos

---
+++ Frontend/React/runoob/06_API/setState.jsx
@@ -0,0 +1,20 @@
+var Counter = React.createClass({
+  getInitialState: function() {
+    return { clickCount: 0 };
+  },
+  handleClick: function() {
+    let s = { clickCount: this.state.clickCount + 1 };
+    this.setState(s);
+    // this.setState(function(state) {
+    //   return { clickCount: state.clickCount + 1 };
+    // });
+  },
+  render: function() {
+    return (
+      <h2 onClick={this.handleClick}>
+        点我！点击次数为: {this.state.clickCount}
+      </h2>
+    );
+  }
+});
+ReactDOM.render(<Counter />, document.getElementById('message'));
