3ec1f13a05f58b68d9d1fd41f6a37949d17e126f
yuchao919 <yuchao919@hotmail.com>
2018-03-17 23:20:14 +0800

    finish redux-thunk demo

--- Frontend/Redux-demo/simple/store.js
+++ Frontend/Redux-demo/simple/store.js
@@ -1,25 +1,36 @@
-import { createStore } from 'redux';
-import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './simple/actions';
-import todoApp from './simple/reducers';
+import { createStore, applyMiddleware } from 'redux';
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions';
+import todoApp from './reducers';
 
-let store = createStore(todoApp, {
-  todos: [{ text: 'Hello world', completed: false }]
-});
+const logger = store => next => action => {
+  console.log('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  return result;
+};
+
+const store = createStore(
+  todoApp,
+  {
+    todos: [{ text: 'Hello world', completed: false }]
+  },
+  applyMiddleware(logger)
+);
 
 // 打印初始状态
-console.log(store.getState());
+// console.log(store.getState());
 
 // 每次 state 更新时，打印日志
 // 注意 subscribe() 返回一个函数用来注销监听器
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
 // 发起一系列 action
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+store.dispatch(addTodo('Learn about actions')); // 返回action生成的对象"{"type":"ADD_TODO","text":"Learn about actions"}"
+// store.dispatch(addTodo('Learn about reducers'));
+// store.dispatch(addTodo('Learn about store'));
+// store.dispatch(toggleTodo(0));
+// store.dispatch(toggleTodo(1));
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
 
 // 停止监听 state 更新
-unsubscribe();
+// unsubscribe();
