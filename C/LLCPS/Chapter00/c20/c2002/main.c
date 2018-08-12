0f4b17aa61e02afc98a46c1d9b3d92763f10b6a7
yuchao919 <yuchao919@hotmail.com>
2018-08-12 22:53:31 +0800

    continue to review LLCPS

---
+++ C/LLCPS/Chapter00/c20/c2002/main.c
@@ -0,0 +1,17 @@
+/* main.c */
+#include <stdio.h>
+#include "stack.h"
+
+int main(void)
+{
+  push('a');
+  push('b');
+  push('c');
+
+  while (!is_empty()) {
+    putchar(pop());
+  }
+  putchar('\n');
+
+  return 0;
+}
---
+++ C/LLCPS/Chapter00/c21/main.c
@@ -0,0 +1,14 @@
+#define MAXX(a, b) ((a)>(b)?(a):(b))
+
+int a[] = { 9, 3, 5, 2, 1, 0, 8, 7, 6, 4 };
+
+int max(int n)
+{
+  return n == 0 ? a[0] : MAX(a[n], max(n - 1));
+}
+
+int main(void)
+{
+  max(9);
+  return 0;
+}
