933c02cd9072b972a16c15255513ef46ffb58c61
yuchao919 <yuchao919@hotmail.com>
2018-09-03 13:19:19 +0800

    .

--- C/LLCPS/Chapter00/test/twoSum.c
+++ C/LLCPS/Chapter00/test/twoSum.c
@@ -7,20 +7,17 @@
 int *twoSum(int *nums, int numsSize, int target)
 {
     int *result = malloc(sizeof(int) * 2);
-	int end = numsSize - 1;
-	for(int i = 0; i < end; ++i)
-	{
-		for(int j = i + 1; j < numsSize; ++j)
-		{
-			if(nums[i] + nums[j] == target)
-			{
-				result[0] = i;
-				result[1] = j;
-				return result;
-			}
-		}
+    int end = numsSize - 1;
+    for (int i = 0; i < end; ++i) {
+	for (int j = i + 1; j < numsSize; ++j) {
+	    if (nums[i] + nums[j] == target) {
+		result[0] = i;
+		result[1] = j;
+		return result;
+	    }
 	}
-	return result;
+    }
+    return result;
 }
 
 int main(void)
