9a2e1578a2476405308ac69c8c5da320bb5e5374
yuchao919 <yuchao919@hotmail.com>
2019-09-20 09:35:43 +0800

    1

--- C#/TestConsoleApp/Program.cs
+++ C#/TestConsoleApp/Program.cs
@@ -8,22 +8,27 @@ namespace TestConsoleApp
 {
     class Program
     {
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        //{
+        //    var result = await innerCalculate(1);
+        //    Console.WriteLine(result);
+
+        //    Console.Read();
+
+        //    async Task<int> innerCalculate(int number)
+        //    {
+        //        return await Calculate(number);
+        //    }
+        //}
+
+        //static async Task<int> Calculate(int number)
+        //{
+        //    return await Task.Run<int>(() => { return number * 2; });
+        //}
+
+        static void Main(string[] args)
         {
-            var result = await innerCalculate(1);
-            Console.WriteLine(result);
-
-            Console.Read();
-
-            async Task<int> innerCalculate(int number)
-            {
-                return await Calculate(number);
-            }
-        }
-
-        static async Task<int> Calculate(int number)
-        {
-            return await Task.Run<int>(() => { return number * 2; });
+           
         }
     }
 }
