1037ffdd5e2e8323668e765ca40fbec7e9d326b1
yuchao919 <yuchao919@hotmail.com>
2019-12-30 22:19:52 +0800

    add SimpleTCP demo

---
+++ C/SimpleTCP/client/client.c
@@ -0,0 +1,52 @@
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+
+// #pragma comment(lib,"ws2_32.lib")
+
+int main(int argc, char* argv[])
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA data;
+	if (WSAStartup(sockVersion, &data) != 0)
+	{
+		return 0;
+	}
+ 
+	SOCKET sclient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sclient == INVALID_SOCKET)
+	{
+		printf("invalid socket !");
+		return 0;
+	}
+ 
+	struct sockaddr_in serAddr;
+	serAddr.sin_family = AF_INET;
+	serAddr.sin_port = htons(8888);
+	serAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	if (connect(sclient, (struct sockaddr *)&serAddr, sizeof(serAddr)) == SOCKET_ERROR)
+	{
+		printf("connect error !");
+		closesocket(sclient);
+		return 0;
+	}
+	for (int i = 0; i < 1000; i++)
+	{
+ 
+		char * sendData = "csi接收  二进制文件未解析\n";
+		send(sclient, sendData, strlen(sendData), 0);
+		char recData[255];
+		/*int ret = recv(sclient, recData, 255, 0);
+		if (ret > 0)
+		{
+	
+			recData[ret] = 0x00;
+			//printf(recData);
+			printf("%d%s",i,recData);
+		}
+		*/
+	}
+	closesocket(sclient);
+	WSACleanup();
+	return 0;
+}
