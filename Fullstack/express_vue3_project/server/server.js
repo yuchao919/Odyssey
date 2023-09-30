fd6876607d93cb97106daf4f3dfc09a3db7e19f0
yuc05 <yuc05@mingyuanyun.com>
2023-09-30 21:28:35 +0800

    add express_vue3_project boilerplate

---
+++ Fullstack/express_vue3_project/server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// const db = require("./server/models");
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Synced db.");
+// }).catch((err) => {
+//     console.log("Failed to sync db: " + err.message);
+// });
+
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to application." });
+});
+
+// require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 58080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
