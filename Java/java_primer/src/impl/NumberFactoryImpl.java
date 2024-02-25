7d62d4638aa005392c76d62a2fad6238525d9e1c
yuchao919 <yuchao919@hotmail.com>
2024-02-25 20:49:04 +0800

    1

---
+++ Java/java_primer/src/impl/NumberFactoryImpl.java
@@ -0,0 +1,7 @@
+package impl;
+
+import interfaces.NumberFactory;
+
+public class NumberFactoryImpl implements NumberFactory {
+
+}
---
+++ Java/java_primer/src/interfaces/NumberFactory.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import impl.NumberFactoryImpl;
+
+public interface NumberFactory {
+    Number parse(String s);
+
+     // 获取工厂实例:
+     static NumberFactory getFactory() {
+        return impl;
+    }
+
+        static NumberFactory impl = new NumberFactoryImpl();
+}
