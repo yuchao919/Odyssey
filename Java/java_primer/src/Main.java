c7f722bb1ba2e085c64a8c2349e38c2a49b33cdd
Chao Yu <yuchao919@hotmail.com>
2024-04-06 18:43:24 +0800

    1

--- Java/java_primer/src/Main.java
+++ Java/java_primer/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import demos.SuperSimpleHttpServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SuperSimpleHttpServer.start(53333);
+        List<UUID> list = new ArrayList<>();
+        UUID a = UUID.fromString("14b10b59-e690-4f43-90a6-a732b6e02a2a");
+        UUID b = UUID.fromString("14b10b59-e690-4f43-90a6-a732b6e02a2a");
+        list.add(a);
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        System.out.println(list.contains(b));
     }
 }
