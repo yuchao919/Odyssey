3e340d39a2dd47c7d3a2ec4f576abb710b1732f0
yuchao919 <yuchao919@hotmail.com>
2024-07-27 02:06:43 +0800

    1

---
+++ Node/my-tools/index.js
@@ -0,0 +1,18 @@
+const { recordFiles } = require("./lib/recordFiles");
+
+
+// (async function main() {
+//   const dirList = ["C:\\Programs\\Hentai", "D:\\Self\\Downloads\\迅雷下载", "E:\\Hentai"];
+//   const options = {
+//     seqConfig: {
+//       dialect: "sqlite",
+//       storage: "D:\\Codes\\yuchao919\\Odyssey\\Node\\my-tools\\db\\test_20240727014755.db",
+//       logging: false
+//     },
+//     tableVersion: Date.now()
+//   };
+
+//   await recordFiles(dirList, options);
+
+//   console.log("Done");
+// })();
---
+++ Node/my-tools/lib/recordFiles.js
@@ -0,0 +1,93 @@
+const { Sequelize, DataTypes, Op } = require('sequelize');
+const { traverseDirectory } = require('./traverseDirectory.js');
+
+
+module.exports = {
+  recordFiles: async function (dirList, options) {
+    const { seqConfig, tableVersion } = options;
+
+    const sqConfig = Object.assign({
+      define: {
+        freezeTableName: true,
+      }
+    }, seqConfig);
+
+    const sequelize = new Sequelize(sqConfig);
+    let suffix = "";
+    if (tableVersion) {
+      suffix += "_" + tableVersion;
+    }
+
+
+    const RfFolder = sequelize.define('RfFolder' + suffix, {
+      Id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      FolderName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      Path: {
+        type: DataTypes.STRING,
+      }
+    }, {});
+
+
+    const RfFile = sequelize.define('RfFile' + suffix, {
+      Id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      FileName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      FolderName: {
+        type: DataTypes.STRING
+      },
+      FolderPath: {
+        type: DataTypes.STRING
+      },
+      Path: {
+        type: DataTypes.STRING
+      }
+    }, {});
+
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+      await sequelize.sync({ force: true });
+      console.log("All models were synchronized successfully.");
+
+
+      // const dirList = ["D:\\Codes\\yuchao919\\Odyssey\\Node\\my-tools\\docs"];
+
+      const folderList = [];
+      const fileList = [];
+
+
+
+      for (const dir of dirList) {
+        await traverseDirectory(dir, async function recordFiles({ isDirectory, entry, folderName, folderPath, fullPath }) {
+          const name = entry.name;
+
+          if (isDirectory) {
+            folderList.push({ FolderName: name, Path: fullPath });
+          } else {
+            fileList.push({ FileName: name, FolderName: folderName, FolderPath: folderPath, Path: fullPath });
+          }
+
+        });
+      }
+
+      await RfFolder.bulkCreate(folderList);
+      await RfFile.bulkCreate(fileList);
+
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+};
