80af62e6ee0b06e955e9a047f0be652697c62604
yuchao919 <yuchao919@hotmail.com>
2024-08-04 14:27:04 +0800

    1

--- Node/my-tools/config/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  version: "20240727002318",
-  seqConfig: {
-    dialect: "sqlite",
-    storage: "D:\\Codes\\yuchao919\\Odyssey\\Node\\my-tools\\db\\test_20240727014755.db",
-    logging: false
-  }
-};
