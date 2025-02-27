4aab34ae79faaf2189525768a424f11c28f93e9f
yuc05 <yuc05@mingyuanyun.com>
2025-02-27 14:36:25 +0800

    add test-playground project

---
+++ Java/test-playground/src/main/java/com/test/App.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+       
+    }
+}
