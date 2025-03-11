c8c168a67013240910edeee1cb64db8254c53f85
yuchao919 <yuchao919@hotmail.com>
2025-03-11 22:14:30 +0800

    externalize configuration

--- Java/springboot/springboot-first/src/main/java/com/odyssey/MyApplication.java
+++ Java/springboot/springboot-first/src/main/java/com/odyssey/MyApplication.java
@@ -1,5 +1,9 @@
 package com.odyssey;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // meta-annotation,combines @SpringBootConfiguration, @EnableAutoConfiguration
 // and @ComponentScan.
-@SpringBootApplication 
+@SpringBootApplication
 public class MyApplication {
 
   // annotation provides “routing” information.
@@ -19,6 +23,12 @@ public class MyApplication {
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(MyApplication.class, args);
+    SpringApplication app = new SpringApplication(MyApplication.class);
+    Properties props = new Properties();
+    // props.setProperty("spring.profiles.active", "dev");
+    // 优先级最低的方式，会被application.properties顶替掉
+    props.setProperty("server.port", "50086");
+    app.setDefaultProperties(props);
+    app.run(args);
   }
 }
